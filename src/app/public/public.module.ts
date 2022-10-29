4f7ba5dbda74ccb4d26b1a7b376bb69474811267
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2022-10-29 17:40:08 -0500

    Feature:login - Adding new components to work with a login page

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent,
+    LoginFormComponent
+  ],
   imports: [
     CommonModule
   ]
