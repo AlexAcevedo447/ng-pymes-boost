9c3b1fd5a30ccd03c810be3b985ad59fc9674e76
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2022-10-29 19:02:47 -0500

    Gathering:routing - Configuring new module for public

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login/login-page/login-page.component';
 
 const publicRoutes:Routes=[
-
+  {path:"", component:LoginPageComponent}
 ]
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     RouterModule.forChild(publicRoutes)
